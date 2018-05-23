6cf1e4f3482b00ebcded6a33b1adafbe6e80acc1
unknown <mimengis@microsoft.com>
2018-05-22 18:55:34 -0700

    Starting a discussion about how to expose the HubLifetimeManager tests

---
+++ test/Microsoft.AspNetCore.SignalR.Common.Tests/DefaultCustomHubLifetimeManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AspNetCore.SignalR.Common.Tests
+{
+    public class CustomHubLifetimeManagerTests : HubLifeTimeManagerTestsBase<MyHub>
+    {
+        public override HubLifetimeManager<MyHub> GetNewHubLifetimeManager()
+        {
+            return new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+        }
+
+        [Fact]
+        public async Task SendConnectionAsyncOnNonExistentConnectionNoops()
+        {
+            var manager = GetNewHubLifetimeManager();
+            await manager.SendConnectionAsync("NotARealConnectionId", "Hello", new object[] { "World" }).OrTimeout();
+        }
+
+        [Fact]
+        public async Task AddGroupOnNonExistentConnectionNoops()
+        {
+            var manager = GetNewHubLifetimeManager();
+            await manager.AddToGroupAsync("NotARealConnectionId", "MyGroup").OrTimeout();
+        }
+
+        [Fact]
+        public async Task RemoveGroupOnNonExistentConnectionNoops()
+        {
+            var manager = GetNewHubLifetimeManager();
+            await manager.RemoveFromGroupAsync("NotARealConnectionId", "MyGroup").OrTimeout();
+        }
+    }
+
+    public class MyHub : Hub
+    {
+
+    }
+
+}
--- test/Microsoft.AspNetCore.SignalR.Common.Tests/HubLifetimeManagerTestsBase.cs
+++ test/Microsoft.AspNetCore.SignalR.Common.Tests/HubLifetimeManagerTestsBase.cs
@@ -1,24 +1,26 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Protocol;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.SignalR.Tests;
 using Xunit;
 
-namespace Microsoft.AspNetCore.SignalR.Tests
+namespace Microsoft.AspNetCore.SignalR.Common.Tests
 {
-    public class DefaultHubLifetimeManagerTests
+    public abstract class HubLifeTimeManagerTestsBase<THub> where THub : Hub
     {
+        public HubLifetimeManager<THub> Manager;
+
+        public abstract HubLifetimeManager<THub> GetNewHubLifetimeManager();
+
         [Fact]
         public async Task SendAllAsyncWritesToAllConnectionsOutput()
         {
             using (var client1 = new TestClient())
             using (var client2 = new TestClient())
             {
-                var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+                var manager = GetNewHubLifetimeManager();
                 var connection1 = HubConnectionContextUtils.Create(client1.Connection);
                 var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
@@ -45,7 +47,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
             using (var client1 = new TestClient())
             using (var client2 = new TestClient())
             {
-                var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+                var manager = GetNewHubLifetimeManager();
                 var connection1 = HubConnectionContextUtils.Create(client1.Connection);
                 var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
@@ -71,7 +73,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
             using (var client1 = new TestClient())
             using (var client2 = new TestClient())
             {
-                var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+                var manager = GetNewHubLifetimeManager();
                 var connection1 = HubConnectionContextUtils.Create(client1.Connection);
                 var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
@@ -97,7 +99,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
             using (var client1 = new TestClient())
             using (var client2 = new TestClient())
             {
-                var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+                var manager = GetNewHubLifetimeManager();
                 var connection1 = HubConnectionContextUtils.Create(client1.Connection);
                 var connection2 = HubConnectionContextUtils.Create(client2.Connection);
 
@@ -107,7 +109,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
                 await manager.AddToGroupAsync(connection1.ConnectionId, "gunit").OrTimeout();
                 await manager.AddToGroupAsync(connection2.ConnectionId, "gunit").OrTimeout();
 
-                await manager.SendGroupExceptAsync("gunit", "Hello", new object[] { "World" }, new []{ connection2.ConnectionId }).OrTimeout();
+                await manager.SendGroupExceptAsync("gunit", "Hello", new object[] { "World" }, new[] { connection2.ConnectionId }).OrTimeout();
 
                 var message = Assert.IsType<InvocationMessage>(client1.TryRead());
                 Assert.Equal("Hello", message.Target);
@@ -123,7 +125,7 @@ namespace Microsoft.AspNetCore.SignalR.Tests
         {
             using (var client = new TestClient())
             {
-                var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
+                var manager = GetNewHubLifetimeManager();
                 var connection = HubConnectionContextUtils.Create(client.Connection);
 
                 await manager.OnConnectedAsync(connection).OrTimeout();
@@ -134,41 +136,6 @@ namespace Microsoft.AspNetCore.SignalR.Tests
                 Assert.Equal("Hello", message.Target);
                 Assert.Single(message.Arguments);
                 Assert.Equal("World", (string)message.Arguments[0]);
-            }
-        }
-
-        [Fact]
-        public async Task SendConnectionAsyncOnNonExistentConnectionNoops()
-        {
-            var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
-            await manager.SendConnectionAsync("NotARealConnectionId", "Hello", new object[] { "World" }).OrTimeout();
-        }
-
-        [Fact]
-        public async Task AddGroupOnNonExistentConnectionNoops()
-        {
-            var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
-            await manager.AddToGroupAsync("NotARealConnectionId", "MyGroup").OrTimeout();
-        }
-
-        [Fact]
-        public async Task RemoveGroupOnNonExistentConnectionNoops()
-        {
-            var manager = new DefaultHubLifetimeManager<MyHub>(new Logger<DefaultHubLifetimeManager<MyHub>>(NullLoggerFactory.Instance));
-            await manager.RemoveFromGroupAsync("NotARealConnectionId", "MyGroup").OrTimeout();
-        }
-
-        private class MyHub : Hub
-        {
-
-        }
-
-        private class MockChannel: Channel<HubMessage>
-        {
-
-            public MockChannel(ChannelWriter<HubMessage> writer = null)
-            {
-                Writer = writer;
             }
         }
     }
